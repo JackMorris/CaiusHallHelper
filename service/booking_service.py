99bc87881702f8b4dda53eeadffaff60a375f1ba
Jack Morris <jack.morris10@gmail.com>
2015-01-14 14:06:48 +0000

    Added caching to booking_service

--- service/booking_service.py
+++ service/booking_service.py
@@ -7,26 +7,34 @@ from model.event import Event
 
 BOOKING_SERVICE_URL = 'https://www.mealbookings.cai.cam.ac.uk/index.php'
 
+_AVAILABLE_EVENTS_CACHE = None
+_ATTENDEE_NAME_CACHE = {}
+_MENU_TEXT_CACHE = {}
+_EVENT_OCCURRING_CACHE = {}
+
 
 def get_available_events():
     """ Find all available events.
     :return: list of Event instances, representing events that exist in the
         booking system
     """
-    browser = raven_service.get_default_authenticated_browser()
-    hall_html = browser.open(BOOKING_SERVICE_URL).read()
-    hall_soup = BeautifulSoup(hall_html)
+    global _AVAILABLE_EVENTS_CACHE
+    if _AVAILABLE_EVENTS_CACHE is None:
+        _AVAILABLE_EVENTS_CACHE = []
+        browser = raven_service.get_default_authenticated_browser()
+        hall_html = browser.open(BOOKING_SERVICE_URL).read()
+        hall_soup = BeautifulSoup(hall_html)
 
-    events = []
-    events_table = hall_soup.find_all('table', {"class": "list"})[1]
-    for event_row in events_table.find_all('td'):
-        event_name = event_row.get_text()
-        event_links = event_row.find_all('a')
-        if len(event_links) > 0:
-            event_link = event_links[0].get('href')
-            event_code = int(re.search('\d+', event_link).group(0))
-            events.append(Event(code=event_code, name=event_name))
-    return events
+        events_table = hall_soup.find_all('table', {"class": "list"})[1]
+        for event_row in events_table.find_all('td'):
+            event_name = event_row.get_text()
+            event_links = event_row.find_all('a')
+            if len(event_links) > 0:
+                event_link = event_links[0].get('href')
+                event_code = int(re.search('\d+', event_link).group(0))
+                event = Event(code=event_code, name=event_name)
+                _AVAILABLE_EVENTS_CACHE.append(event)
+    return _AVAILABLE_EVENTS_CACHE[:]
 
 
 def get_attendee_names(event, date):
@@ -45,19 +53,23 @@ def get_attendee_names(event, date):
             return attendee_text
         return None
 
-    if not is_event_occurring(event, date):
-        error_string = '%s not occurring on %s' % (str(event), str(date))
-        raise BookingServiceError(error_string)
+    cache_key = (event.code, date)
+    if cache_key not in _ATTENDEE_NAME_CACHE:
+        if not is_event_occurring(event, date):
+            error_string = '%s not occurring on %s' % (str(event), str(date))
+            raise BookingServiceError(error_string)
 
-    browser = raven_service.get_default_authenticated_browser()
-    event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
-    event_html = browser.open(event_url).read()
-    event_soup = BeautifulSoup(event_html)
+        browser = raven_service.get_default_authenticated_browser()
+        event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
+        event_html = browser.open(event_url).read()
+        event_soup = BeautifulSoup(event_html)
 
-    attendance_table = event_soup.find_all('table', {'class': 'list'})[0]
-    attendee_cells = attendance_table.find_all('td')
-    attendee_names = map(get_attendee_name, attendee_cells)
-    return [attendee for attendee in attendee_names if attendee is not None]
+        attendance_table = event_soup.find_all('table', {'class': 'list'})[0]
+        attendee_cells = attendance_table.find_all('td')
+        attendee_names = map(get_attendee_name, attendee_cells)
+        attendee_names = [name for name in attendee_names if name is not None]
+        _ATTENDEE_NAME_CACHE[cache_key] = attendee_names
+    return _ATTENDEE_NAME_CACHE[cache_key][:]
 
 
 def get_menu_text(event, date):
@@ -68,25 +80,28 @@ def get_menu_text(event, date):
         no menu is found
     :raises: BookingServiceError is `event` isn't occurring on `date`
     """
-    if not is_event_occurring(event, date):
-        error_string = '%s not occurring on %s' % (str(event), str(date))
-        raise BookingServiceError(error_string)
-    browser = raven_service.get_default_authenticated_browser()
-    event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
-    event_html = browser.open(event_url).read()
-    event_soup = BeautifulSoup(event_html)
+    cache_key = (event.code, date)
+    if cache_key not in _MENU_TEXT_CACHE:
+        if not is_event_occurring(event, date):
+            error_string = '%s not occurring on %s' % (str(event), str(date))
+            raise BookingServiceError(error_string)
+        browser = raven_service.get_default_authenticated_browser()
+        event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
+        event_html = browser.open(event_url).read()
+        event_soup = BeautifulSoup(event_html)
 
-    menu_divs = event_soup.find_all('div', {'class': 'menu'})
-    if len(menu_divs) == 0:
-        return None
-    menu_text = menu_divs[0].get_text().replace('\r', '\n')
+        menu_divs = event_soup.find_all('div', {'class': 'menu'})
+        if len(menu_divs) == 0:
+            return None
+        menu_text = menu_divs[0].get_text().replace('\r', '\n')
 
-    # Menu text sometimes contains large spans of ' ', so remove them. Also
-    # remove stray spaces at the start/end of a line.
-    menu_text = re.sub(r'  +', '', menu_text)
-    menu_text = re.sub(r' ?\n ?', r'\n', menu_text)
-    menu_text = re.sub(r'(^\n)|(\n$)', r'', menu_text)
-    return menu_text
+        # Menu text sometimes contains large spans of ' ', so remove them. Also
+        # remove stray spaces at the start/end of a line.
+        menu_text = re.sub(r'  +', '', menu_text)
+        menu_text = re.sub(r' ?\n ?', r'\n', menu_text)
+        menu_text = re.sub(r'(^\n)|(\n$)', r'', menu_text)
+        _MENU_TEXT_CACHE[cache_key] = menu_text
+    return _MENU_TEXT_CACHE[cache_key]
 
 
 def is_event_occurring(event, date):
@@ -95,10 +110,14 @@ def is_event_occurring(event, date):
     :param date: datetime.date for the date to check
     :return: bool, True if `event` is occurring on `date`
     """
-    browser = raven_service.get_default_authenticated_browser()
-    event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
-    event_html = browser.open(event_url).read()
-    return 'not running on' not in event_html
+    cache_key = (event.code, date)
+    if cache_key not in _EVENT_OCCURRING_CACHE:
+        browser = raven_service.get_default_authenticated_browser()
+        event_url = event.url_for_date(date, BOOKING_SERVICE_URL)
+        event_html = browser.open(event_url).read()
+        event_occurring = 'not running on' not in event_html
+        _EVENT_OCCURRING_CACHE[cache_key] = event_occurring
+    return _EVENT_OCCURRING_CACHE[cache_key]
 
 
 def create_booking(event, user, date):
